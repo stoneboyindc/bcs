3bb5c23cec2ca7798f2d28a4584c8bf3918a1fe8
Nick Chang <nick.chang@serimaconsulting.com>
2019-03-30 16:23:53 -0400

    Change <br> to <p>

--- Application.php
+++ Application.php
@@ -38,11 +38,11 @@
 			<div id="post-5" class="post-5 page type-page status-publish has-post-thumbnail hentry p publish author-webdev untagged is-full comments-closed pings-closed y2015 m03 d16 h16 alt slug-home">
 			<h1 class="entry-title">Application</h1>
 				<div class="entry-content">
-      We appreciate your interest in Bethesda Community School.  We will begin accepting applications for the 2019-2020 school year on October 1, 2018.  Applications will be processed in the order in which they are received with priority given to returning families, neighbors of BCS and MCPS employees. Children applying must be at least two years of age by September 1, 2019 and we strongly encourage you to visit Bethesda Community School before submitting an application.  Tours are given M-F between 9:00 a.m. and 11:00 a.m.  Please call the school to schedule a tour 301-652-0117.<br>
-        <br>
-        You will be notified if there is a space available for your child/children by the end of February.  Once notified, you will have 1 week to accept the spot and come into the school to fill out the required paperwork for enrollment. There is a $200 deposit/per child due at the time of enrollment.  $50 of the deposit is a non-refundable registration fee. The remaining $150 is also non-refundable, but will be applied towards your first monthâ€™s tuition. <br>
-        <br>
-      Please use the link below to print out a 2018-2019  application.
+      <p>We appreciate your interest in Bethesda Community School.  We will begin accepting applications for the 2019-2020 school year on October 1, 2018.  Applications will be processed in the order in which they are received with priority given to returning families, neighbors of BCS and MCPS employees. Children applying must be at least two years of age by September 1, 2019 and we strongly encourage you to visit Bethesda Community School before submitting an application.  Tours are given M-F between 9:00 a.m. and 11:00 a.m.  Please call the school to schedule a tour 301-652-0117.<br>
+        </p>
+        <p>You will be notified if there is a space available for your child/children by the end of February.  Once notified, you will have 1 week to accept the spot and come into the school to fill out the required paperwork for enrollment. There is a $200 deposit/per child due at the time of enrollment.  $50 of the deposit is a non-refundable registration fee. The remaining $150 is also non-refundable, but will be applied towards your first month's tuition. <br>
+        </p>
+      <p>Please use the link below to print out a 2018-2019  application.</p>
       <p><a href="files/application-2019.pdf" target="_blank">2018-2019 Application</a><br>
       </p>
   </div>
